da234ecbf0813dc2315f263346725c0eb4b85aac
HH <hans.herzl@live.com>
2018-10-28 18:05:47 -0600

    Addition of unit tests for API

---
+++ SourceCode/Backend/API/API.UnitTests/RepositoryMocker.cs
@@ -0,0 +1,25 @@
+﻿using API.Core.DataLayer;
+using API.Core.DataLayer.Contracts;
+using API.Core.DataLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace API.UnitTests
+{
+    public static class RepositoryMocker
+    {
+        public static IWarehouseRepository GetWarehouseRepository(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<CodeChallengeDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            var dbContext = new CodeChallengeDbContext(options);
+
+            dbContext.SeedInMemory();
+
+            return new WarehouseRepository(dbContext);
+        }
+    }
+}
---
+++ SourceCode/Backend/API/API.UnitTests/WarehouseUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using API.Controllers;
+using API.Core.EntityLayer.Warehouse;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace API.UnitTests
+{
+    public class WarehouseUnitTests
+    {
+        [Fact]
+        public async Task TestGetProductsAsync()
+        {
+            // Arrange
+            var repository = RepositoryMocker.GetWarehouseRepository(nameof(TestGetProductsAsync));
+            var controller = new WarehouseController(repository, null);
+
+            // Act
+            var response = await controller.GetProductsAsync() as ObjectResult;
+            var value = response.Value as IPagedResponse<Product>;
+
+            // Assert
+            Assert.False(value.DidError);
+            Assert.True(value.Model.Count() > 0);
+        }
+    }
+}
