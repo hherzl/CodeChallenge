6fda819e8516725b30a6ca1be1feefadd275c3bb
HH <hans.herzl@live.com>
2018-11-04 23:13:28 -0600

    Addition of client execution for run script

--- SourceCode/run.bat
+++ SourceCode/run.bat
@@ -1,11 +1,12 @@
 cls
 set initialPath=%cd%
 set authApiPath=%initialPath%\Backend\API\AuthAPI
+set clientPath=%initialPath%\Frontend\snacks
 cd %authApiPath%
 start dotnet run
-echo AuthAPI is listening on port number 5600
 set apiPath=%initialPath%\Backend\API\API
 cd %apiPath%
 start dotnet run
-echo API is listening on port number 5700
+cd %clientPath%
+start ng serve
 cd %initialPath%
